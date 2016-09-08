8574c090de48cb49159ed885441d188bed7eea61
dragon <divyanshu_j01@yahoo.in>
2016-09-08 09:44:58 +0530

    Update game.py

--- game.py
+++ game.py
@@ -85,3 +85,7 @@ def chooseRandomMoveFromList(board, movesList):
 		if isSpaceFree(board, i):
 			possibleMoves.append(i)
 
+	if len(possibleMoves) != 0:
+		return random.choice(possibleMoves)
+	else:
+		return None
